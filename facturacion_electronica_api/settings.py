e29a73c6d56244b9a128f8feea5c57684e1ba2ce
David Basantes <sebastian2405lucero@hotmail.com>
2025-02-22 21:55:55 -0500

    feat: update static file settings and remove Heroku collectstatic configuration

--- facturacion_electronica_api/settings.py
+++ facturacion_electronica_api/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Directorio para collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Directorios adicionales
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
